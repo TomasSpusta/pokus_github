be400b8265781296bd7b328ae05537a8d157023c
ImpaledTom <pokusik1@gmail.com>
2022-08-18 12:51:40 +0200

    asdf

--- github_braches.py
+++ github_braches.py
@@ -2,6 +2,6 @@ import git
 
 
 remoteurl = "https://github.com/TomasSpusta/pokus_github.git"
-localfolder = "D:\PC\Tom\_Odrive\odrive\GD_Work\_WORK\_Bastl\PiPi_RFID ctecka ceitec nano\pokus_github"
+localfolder = "/pokus_github"
 
 myrepo = git.Repo.init (remoteurl, localfolder)

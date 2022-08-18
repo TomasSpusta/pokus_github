011601076997fe3a69884968a75f1191cd7813f0
ImpaledTom <pokusik1@gmail.com>
2022-08-18 12:50:40 +0200

    dsfa

--- github_braches.py
+++ github_braches.py
@@ -1,3 +1,7 @@
 import git
 
 
+remoteurl = "https://github.com/TomasSpusta/pokus_github.git"
+localfolder = "D:\PC\Tom\_Odrive\odrive\GD_Work\_WORK\_Bastl\PiPi_RFID ctecka ceitec nano\pokus_github"
+
+myrepo = git.Repo.init (remoteurl, localfolder)
